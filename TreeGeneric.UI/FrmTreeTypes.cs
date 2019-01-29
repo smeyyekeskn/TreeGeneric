c8a70fda8e5c14b8529a324b4ad3829783630c9c
Sümeyye Keskin <smeyyekeskn@gmail.com>
2019-01-29 15:00:40 +0300

    Change delete button function on FrmTreeTypes

--- TreeGeneric.UI/FrmTreeTypes.cs
+++ TreeGeneric.UI/FrmTreeTypes.cs
@@ -62,6 +62,7 @@ namespace TreeGeneric.UI
                 int id = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
                 treeTypeService.Delete(id);
                 MessageBox.Show("Kayıt Başarıyla Silindi");
+                MessageBox.Show("Liste Güncellendi.");
                 RefreshGrid();
             }
             else
